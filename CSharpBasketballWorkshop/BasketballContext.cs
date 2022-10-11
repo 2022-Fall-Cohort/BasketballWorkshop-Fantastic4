00b962c224751c80177ebfd8b4d8d78922b2b00a
SamanthaJ2 <samanthamariejae@gmail.com>
2022-10-11 14:07:33 -0400

    finalcommit

---
+++ CSharpBasketballWorkshop/BasketballContext.cs
@@ -0,0 +1,27 @@
+﻿using CSharpBasketballWorkshop.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CSharpBasketballWorkshop
+{
+    public class BasketballContext : DbContext
+    {
+        public DbSet<Player>? Players { get; set; }
+        public DbSet<Team>? Teams { get; set; }
+        //public BasketballContext(DbContextOptions<BasketballContext> options) : base(options)
+        //{ }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=BasketballDB;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString).UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
